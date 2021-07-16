62d977eaac0a07e9b34c10ef46674428448c3438
mahendranviji333 <mahendranviji333.gmail.com>
2021-07-16 20:52:00 +0530

    added the journal and article files

--- Zealbot/Publisher details/sample.py
+++ Zealbot/Publisher details/sample.py
@@ -14,8 +14,7 @@ agent = {"User-Agent": rm_agent}
 # url = input1
 
 num =0
-input1 =sys.argv[1]
-url = input1
+url = 'https://brill.com/view/journals/daph/daph-overview.xml?rskey=FAVN5a&result=89'
 print('<><>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
 print(url)
 print('<><>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
@@ -23,12 +22,14 @@ page = requests.get(url, headers=agent)
 soup = BeautifulSoup(page.content,'lxml')
 # Article = soup.find('div',attrs={'class' : 'display-none display@xs-none display@sm-none display@md-flex flex@md-row flex@md-nowrap mr-4'})
 Total_article = soup.find('div',attrs={'id' : 'container-45453-item-45472'})
+
 sub_con = Total_article.find_all('div',attrs={'class':'configurable-layout w-1'})
 
 for i in sub_con:
     num +=1
     # title1 = i.find('div',attrs={'class':'content-box-body null'})
     print('Artical Number ==>',num)
+   
     title = i.find('h6',attrs={'class':'typography-body title mb-3 text-headline font-content'})
     if title is None :
         title1 = i.find('h6',attrs={'class':'typography-body title text-headline font-content'})
@@ -39,3 +40,5 @@ for i in sub_con:
         print(title.text)
         url = title.find('a')
         print('https://brill.com/'+(url['href']))
+
+    
